85fa3506378ea6dd582cbc8d1e5c629375ac3bac
mmikishev <mmikishev@mail.ru>
2021-03-03 19:29:47 +0400

    Загрузка файлов на github [#478904]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+     public static void main(String[] args){
+         System.out.println("Max Mikishev");
+         System.out.println("12.05.2030");
+
+     }
+}
