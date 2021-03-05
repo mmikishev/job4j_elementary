8a8d2c21c7e8ec87063a5918e69dc98afb61ab59
mmikishev <mmikishev@mail.ru>
2021-03-05 11:23:15 +0400

    4.1. Аргументы [#478889]

--- src/ru/job4j/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/ru/job4j/calculator/Fit.java
@@ -3,13 +3,13 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        double rsl = (height-100)*1.15;
-        return rsl;
+        double rsl = -1;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height-110)*1.15;
-        return rsl;
+        double rsl = -1;
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
