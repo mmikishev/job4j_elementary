00bfa16eb20fe5cd9d4184b71afe47863d930b61
mmikishev <mmikishev@mail.ru>
2021-03-16 11:50:26 +0400

    4.2. Тернарное сравнение [#478838]

--- src/ru/job4j/ru/job4j/condition/Max.java
+++ src/ru/job4j/ru/job4j/condition/Max.java
@@ -2,7 +2,6 @@ package ru.job4j.ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = left > right;
         return left > right ? left : right;
     }
 }
