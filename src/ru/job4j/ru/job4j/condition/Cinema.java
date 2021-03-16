ab8405293a0143cf3aa7be9acd9736a356cc8da8
mmikishev <mmikishev@mail.ru>
2021-03-16 19:02:44 +0400

    4.3.1. Оператор && [#478831]

--- src/ru/job4j/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/ru/job4j/condition/Cinema.java
@@ -2,7 +2,7 @@ package ru.job4j.ru.job4j.condition;
 
 public class Cinema {
     public static void permission(boolean allowByParent, boolean hasMoney) {
-        if (true) {
+        if (allowByParent && hasMoney) {
             System.out.println("I can go to the cinema.");
         } else {
             System.out.println("I can't.");
